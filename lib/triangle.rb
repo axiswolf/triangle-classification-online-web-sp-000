96cd4ef587872ced8f511e2ab9b77b2ccdc5d098
Karen V <karenvo@usc.edu>
2019-05-07 08:42:44 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,36 +1,4 @@
-# class Triangle
-#   # write code here
-#   attr_accessor :length_a, :length_b, :length_c
-#
-#   def initialize(length_a, length_b, length_c)
-#     @length_a = length_a
-#     @length_b = length_b
-#     @length_c = length_c
-#   end
-#   def kind
-#     # raise a custom error Triangle Error if triangle is invalid
-#     if length_a == length_b && length_a == length_c
-#       :equilateral
-#     elsif length_a == length_b || length_a == length_c
-#       :isosceles
-#     else
-#       :scalene
-#     end
-#   end
-#   def isvalid?
-#     # if length_a or length_b or length_c == 0
-#     #
-#     # elsif length_a
-#     # triangle with no sides are invalid
-#     # triangle with a negative side is invalid
-#   end
-#   class TriangleError < StandardError
-#     # inherit from StandardError
-#   end
-# end
-
 class Triangle
-  # write code here\
   attr_accessor :s1, :s2, :s3
   def initialize(s1, s2, s3)
     @s1 = s1

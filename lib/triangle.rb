abc4800a2da2c1958fdf6963ca8aea5a38e64815
Karen V <karenvo@usc.edu>
2019-05-07 08:42:30 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -33,15 +33,6 @@ class Triangle
   # write code here\
   attr_accessor :s1, :s2, :s3
   def initialize(s1, s2, s3)
-    # if s1 >= (s2 + s3) || s2 >= (s1 + s3) || s3 >= (s1 + s2)
-    #   raise TriangleError
-    # end
-    # if s1 <= 0 || s2 <= 0 ||s3 <= 0
-    #   raise TriangleError
-    # end
-    # if s1 == nil || s2 == nil ||s3 == nil
-    #   raise TriangleError
-    # end
     @s1 = s1
     @s2 = s2
     @s3 = s3
